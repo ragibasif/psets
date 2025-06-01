84bf5eafa45a9a7f75dcb5a4d88b94e701237fe9
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-06-01 13:01:23 -0400

    backup: automated update Sun Jun  1 13:01:23 2025

---
+++ leetcode/easy/1929_concatenation_of_array/py/main.py
@@ -0,0 +1,20 @@
+"""
+* File: main.py
+* Author: Ragib Asif
+* Email: ragib.asif30@myhunter.cuny.edu
+* GitHub: https://github.com/ragibasif
+* LinkedIn: https://www.linkedin.com/in/ragibasif/
+* SPDX-License-Identifier: MIT
+* Copyright (c) 2025 Ragib Asif
+* Version 1.0.0
+*
+* Problem: 1929 - concatenation_of_array
+* Platform: leetcode
+* Difficulty: easy
+* URL: https://leetcode.com/problems/concatenation-of-array/
+"""
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
