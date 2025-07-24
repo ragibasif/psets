0ec797511a8169a1419a92f60f1866f2eb12afaf
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-07-24 14:16:14 -0400

    feat(cses): add coin_piles code

--- README.md
+++ README.md
@@ -15,17 +15,8 @@ cp
   <a href="https://github.com/ragibasif/cp/stargazers">
     <img alt="Stars" src="https://img.shields.io/github/stars/ragibasif/cp?style=for-the-badge&logo=apachespark&color=D29922&logoColor=C9D1D9&labelColor=0D1117">
   </a>
-  <a href="https://github.com/ragibasif/cp/network/members">
-    <img alt="Forks" src="https://img.shields.io/github/forks/ragibasif/cp?style=for-the-badge&logo=github&color=3FB950&logoColor=C9D1D9&labelColor=0D1117">
-  </a>
-  <a href="https://github.com/ragibasif/cp/actions">
-    <img alt="Build Status" src="https://img.shields.io/github/actions/workflow/status/ragibasif/cp/build.yml?branch=master&style=for-the-badge&label=build&logo=githubactions&color=58A6FF&logoColor=C9D1D9&labelColor=0D1117">
-  </a>
   <a href="https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2Fragibasif%2Fcp">
     <img alt="Visitors" src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2Fragibasif%2Fcp&label=visitors&labelColor=%230D1117&countColor=%2358A6FF">
-  </a>
-  <a href="https://github.com/ragibasif/cp/issues">
-    <img alt="Issues" src="https://img.shields.io/github/issues/ragibasif/cp?style=for-the-badge&logo=bilibili&color=F85149&logoColor=C9D1D9&labelColor=0D1117">
   </a>
   <a href="https://github.com/ragibasif/cp/blob/master/LICENSE">
     <img alt="License" src="https://img.shields.io/github/license/ragibasif/cp?style=for-the-badge&logo=starship&color=8957E5&logoColor=C9D1D9&labelColor=0D1117">
---
+++ cses/introductory/1754_coin_piles/cpp/main.cpp
@@ -0,0 +1,169 @@
+/*
+ * File: main.cpp
+ * Author: Ragib Asif
+ * GitHub: https://github.com/ragibasif
+ * LinkedIn: https://www.linkedin.com/in/ragibasif/
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 Ragib Asif
+ * Version 1.0.0
+ *
+ * Problem: 1754 - coin_piles
+ * Platform: cses
+ * Difficulty: introductory
+ * URL: https://cses.fi/problemset/task/1754
+ */
+
+/*----------------------------------------------------------------------------*/
+// INCLUDES
+/*----------------------------------------------------------------------------*/
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*----------------------------------------------------------------------------*/
+// FILE INPUT/OUTPUT
+/*----------------------------------------------------------------------------*/
+
+#define IO(NAME)                                                               \
+    do {                                                                       \
+        cin.tie(0)->sync_with_stdio(0);                                        \
+        if (fopen(NAME ".in", "r")) {                                          \
+            freopen(NAME ".in", "r", stdin);                                   \
+            freopen(NAME ".out", "w", stdout);                                 \
+        }                                                                      \
+    } while (0)
+
+/*----------------------------------------------------------------------------*/
+// CONSTANTS
+/*----------------------------------------------------------------------------*/
+
+const int           MOD   = 1e9 + 7;
+const int           INF   = 1e9;
+const long long int LLINF = 4e18;
+const double        PI    = acos(-1.0);
+const int           dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+
+/*----------------------------------------------------------------------------*/
+// Debugging Helpers
+/*----------------------------------------------------------------------------*/
+
+template <typename T> void dbg(const T &n) { cerr << n << endl; }
+
+template <typename T1, typename T2> void dbg(const pair<T1, T2> &n) {
+    cerr << "pair = [" << n.first << ", " << n.second << "]" << endl;
+}
+
+template <typename T> void dbg(const vector<T> &n) {
+    cerr << "vector = [" << endl;
+    bool first = true;
+    for (const auto &x : n) {
+        if (!first) {
+            cerr << ", " << endl;
+        }
+        cerr << "    " << x;
+        first = false;
+    }
+    cerr << endl << "]" << endl;
+}
+
+template <typename T> void dbg(const unordered_set<T> &n) {
+    cerr << "unordered_set = {" << endl;
+    bool first = true;
+    for (const auto &x : n) {
+        if (!first) {
+            cerr << ", " << endl;
+        }
+        cerr << "    " << x;
+        first = false;
+    }
+    cerr << endl << "}" << endl;
+}
+
+template <typename T1, typename T2> void dbg(const unordered_map<T1, T2> &n) {
+    cerr << "unordered_map = {" << endl;
+    bool first = true;
+    for (const auto &x : n) {
+        if (!first) {
+            cerr << ", " << endl;
+        }
+        cerr << "    [" << x.first << ", " << x.second << "]";
+        first = false;
+    }
+    cerr << endl << "}" << endl;
+}
+
+/*----------------------------------------------------------------------------*/
+// SOLVE
+/*----------------------------------------------------------------------------*/
+
+int solve(void) {
+
+    unsigned long long int n;
+    if (!(cin >> n)) {
+        return EXIT_FAILURE;
+    }
+    for (unsigned long long int i = 0; i < n; i++) {
+        unsigned long long int a, b;
+        cin >> a >> b;
+
+        if (a == 0 || b == 0) {
+            if (a == 0 && b == 0) {
+                cout << "YES\n";
+            } else {
+                cout << "NO\n";
+            }
+        } else if (((a == 2 * b) || (b == 2 * a)) && ((a + b) % 3 == 0)) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+
+    return EXIT_SUCCESS;
+} // solve
+
+/*----------------------------------------------------------------------------*/
+// MAIN
+/*----------------------------------------------------------------------------*/
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    t = 1;
+    // cin >> t; // Uncomment for multiple test cases
+    for (int i = 0; i < t; i++) {
+        if (solve()) {
+            break;
+        }
+    }
+
+    cerr << endl
+         << "Finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+
+    return EXIT_SUCCESS;
+} // main
